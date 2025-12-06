74bab74e068a8351cd13ab760fb532f18c776a13
ThickMind <fahd4alj@gmail.com>
2025-12-06 15:54:46 +0400

    Update replit.nix

--- replit.nix
+++ replit.nix
@@ -1,23 +1,6 @@
 { pkgs }: {
   deps = [
-    pkgs.python310
-    pkgs.python310Packages.pip
-    pkgs.xorg.libX11
-    pkgs.xorg.libXtst
-    pkgs.xorg.libXext
-    pkgs.xorg.libXrandr
-    pkgs.tk
-    pkgs.tcl
-    pkgs.xdotool
-    pkgs.xvfb-run
+    pkgs.python311
+    pkgs.python311Packages.pip
   ];
-  env = {
-    PYTHON_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-      pkgs.xorg.libX11
-      pkgs.xorg.libXtst
-      pkgs.xorg.libXext
-      pkgs.xorg.libXrandr
-    ];
-    DISPLAY = ":99";
-  };
 }
